12a83fdf46274c7c19db0423c2dc95d2b7119d27
harshada <11harshadapatil@gmail.com>
2024-07-16 18:20:34 +0530

    complete profile page functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -5,6 +5,9 @@ import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/st
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import { app } from '../firebase';
+import { updateStart,updateSuccess,updateFailure } from '../redux/user/user.Slice.js';
+import { useDispatch } from 'react-redux';
+
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
@@ -12,7 +15,14 @@ export default function DashProfile() {
   const [imageFileUrl,setImageFileUrl]=useState(null);
   const [imageFileUploadProgress,setImageFileUploadProgress]=useState(null);
   const [imageFileUploadError,setImageFileUploadError]=useState(null);
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+  const [updateUserError, setUpdateUserError] = useState(null);
+  const [formData,setFormData]=useState({});
+const dispatch=useDispatch();
+
 const filePickerRef=useRef();
+
   function handleImageChange(e){
     const file=e.target.files[0];
     if(file){
@@ -29,6 +39,7 @@ useEffect(()=>{
 },[imageFile]);
 
   const uploadImage=async ()=>{
+    setImageFileUploading(true);
   const storage=getStorage(app); //app imported from firebase.js
   const fileName=new Date().getTime()+imageFile.name;
   const storageRef=ref(storage,fileName);
@@ -44,19 +55,78 @@ useEffect(()=>{
       setImageFileUploadProgress(null);
       setImageFile(null);
       setImageFileUrl(null);
+      setImageFileUploading(false);
     },
     ()=>{
       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
-        setImageFileUrl(downloadURL);
-      })
+      setImageFileUrl(downloadURL);
+      setFormData({...formData,profilePicture: downloadURL});
+      setImageFileUploading(false);
+      });
     }
 
-  )
+  );
+  }
+
+  const handleChange=(e)=>{
+    setFormData({...formData,[e.target.id]:e.target.value});
+   console.log(formData);
+   
+
+  };
+
+const handleSubmit=async (e)=>{
+e.preventDefault();
+console.log(updateUserError);
+if(Object.keys(formData).length===0){
+  console.log("no changes");
+  setUpdateUserError('No changes made');
+  return;
+}
+if(imageFileUploading){
+  setUpdateUserError('Please wait for image to upload');
+  return;
+}
+try{
+  dispatch(updateStart());
+  const res=await fetch(`/api/user/update/${currentUser._id}`,{
+    method:'PUT',
+    headers:{
+      'Content-Type':'application/json',
+
+    },
+    body:JSON.stringify(formData),
+  });
+  // const res = await fetch(`/api/user/update/${currentUser._id}`, {
+  //   method: 'PUT',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify(formData),
+  // });
+const data= await res.json();
+console.log(data);
+if(data.success===false){
+  dispatch(updateFailure(data.message));
+  setUpdateUserError(data.message);
+}
+else{
+dispatch(updateSuccess(data));
+console.log("successfully inserted data");
+setUpdateUserSuccess("users profile updated successfully");
+}
+}
+
+catch(error){
+
+dispatch(updateFailure(error.message));
+setUpdateUserError(error.message);
+}
   }
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/>
         <div className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
           {
@@ -101,14 +171,16 @@ useEffect(()=>{
           id='username'
           placeholder='username'
           defaultValue={currentUser.username}
+          onChange={handleChange}
         />
         <TextInput
           type='email'
           id='email'
           placeholder='email'
           defaultValue={currentUser.email}
+          onChange={handleChange}
         />
-        <TextInput type='password' id='password' placeholder='password' />
+        <TextInput type='password' id='password' placeholder='password'  onChange={handleChange} />
         <Button type='submit' gradientDuoTone='purpleToBlue' outline>
             Update
         </Button>
@@ -117,6 +189,17 @@ useEffect(()=>{
         <span className='cursor-pointer'>Delete Account</span>
         <span className='cursor-pointer'>Sign Out</span>
       </div>
+      {
+        updateUserSuccess && (<Alert color='success' className='mt-5' >
+          {updateUserSuccess}
+        </Alert>)
+      }
+      {
+        updateUserError && (<Alert color='failure' className='mt-5' >
+          {updateUserError}
+        </Alert>
+        )
+      }
     </div>
   );
 }
--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -5,9 +5,10 @@ export default function ThemeProvider({children}) {
     const {theme}=useSelector((state)=>state.theme);
   return (
     <div className={theme}>
-    <div className='bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(16,23,42)] '>
+    <div className=' text-gray-200 bg-[rgb(16,23,42)] '>
       {children}
     </div>
   </div>
   )
 }
+//bg-white text-gray-700
